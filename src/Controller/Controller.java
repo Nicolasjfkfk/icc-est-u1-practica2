13324d2d09800f65dfedb589c4e5dd0447e7089f
Nicolas <nico01pomaquiza@gmail.com>
2024-10-21 12:25:50 -0500

    lol2

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -15,7 +15,38 @@ public class Controller {
         this.sortingMethods = sortingMethods;
         this.searchMethods = searchMethods;
         
-      System.out.println("CONTROLLER CREATE");
+      System.out.println(Iniciando);
+
+      public void star (){
+        int option = 0;
+        do{
+            option = view.showMenu();
+            switch (option) {
+                case 1:
+                    inputPersons()
+                   break;
+                case 2:
+                    addPerson()
+                   break;
+                case 100:
+                   System.out.println("ADIOS");
+                   break;
+                default:
+                System.out.println("ingrese otra opcion");
+                   
+            }
+        }while (opcion !=0);
     }
-    
+
+    public void inputPersons(){
+    int numeroPersonas = view.inputInt("INGRESE EL NUMERO DE PERSONAS: ");
+    personas = new person[numeroPersonas];
+    for (int i =0; i<numeroPersonas; i++){
+        personas[i] = view.inputPerson();
+      }
+    }
+
+    public void addPerson(){
+        int numeroPersonas = view.inputInt("INGRESE EL NUMERO DE PERSONAS a adicionar: ");
+  } 
 }
--- src/Views/View.java
+++ src/Views/View.java
@@ -1,6 +1,7 @@
 package Views;
 
 import java.util.Scanner;
+import models.Person;
 
 public class View {
 
@@ -11,10 +12,30 @@ public class View {
         System.out.println("MENU");
         System.out.println("1. Ingresar persona");
         System.out.println("1OO SALIR");
-        return scanner.nextInt()
+        return scanner.nextInt();
     }
     public int inputInt(String message){
         System.out.println(message);
+        return scanner.nextInt();
+    }
+
+    public Person inputPerson(){
+        String name = inputName();
+        int age = inputAge();
+        return new Person(name, age);
+        
+    }
+    public String inputName(){
+        System.out.println("INGRESE EL NOMBRE: ");
+        return scanner.next();
+    }
+
+    public int inputAge(){
+        return inputInt("ingrese la edad");
+    }
+
+    public int inputInt(String message){
+        System.out.println("Ingrese la edad: ");
         return scanner.nextInt();
     }
     
